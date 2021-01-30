74431ffec364a72372258ad7df3eea7befc86031
Joshua Miller <joshdan65@gmail.com>
2021-01-30 13:36:00 -0600

    Cleanup and fix tagging
    * Remove external dependency from Tagging module
    * Switch credential to pass basic auth header instead of header used for
      oauth app ids

--- tasks/tag/tag.ts
+++ tasks/tag/tag.ts
@@ -1,5 +1,3 @@
-import tl = require('azure-pipelines-task-lib/task');
-
 export interface PersonalAccessToken {
     Value: string
     Type: 'PersonalAccessToken'
@@ -19,42 +17,67 @@ export interface TagOptions {
     ForcePush: boolean
 }
 
-function buildCredentialsFunction(credentials: GitTagCredentials) {
+function buildCredentials(credentials: GitTagCredentials) {
     if (credentials.Type == 'PersonalAccessToken') {
         console.log("Using PersonalAccessToken for Auth.")
-        return function () {
-            return function() {return `x-oauth-basic: ${credentials.Value}`;}
+        return `AUTHORIZATION: Basic ${credentials.Value}`;
+    }
+
+    // if (credentials.Type == 'PersonalAccessToken') {
+    //     console.log("Using PersonalAccessToken for Auth.")
+    //     return `x-oauth-basic: ${credentials.Value}`;
+    // }
+
+    console.log("Using no credentials. This most likely will fail, as pushing tags should require credentials...")
+    return "";
+}
+
+export type ExecutableFunction = (toolName: string, args: string | string[]) => Q.Promise<number>;
+
+type FailureResponse = {
+    Succeeded: false,
+    FailedCommand: string
+    Message: string
+}
+
+type SuccessResponse = {
+    Succeeded: true,
+}
+
+type Response = SuccessResponse | FailureResponse;
+
+export async function addTag(options: TagOptions, executor: ExecutableFunction): Promise<Response> {
+    const repoUrl = `${options.ServiceUrl}/${options.RepoId}.git`;
+
+    const extraHeader = buildCredentials(options.Credentials);
+
+    const forceCommand = options.ForcePush === true ? " --force" : "";
+    const tagMessageCommand = options.TagMessage === "" ? "" : ` -m "${options.TagMessage}"`;
+    const orderedCommands = [
+        { name: "init", command: "init" },
+        { name: "name", command: `config user.name "Universal Git Tags"` },
+        { name: "email", command: "config user.email universal-git-tags@cloudpup.dev" },
+        { name: "extra header", command: `config http.extraheader "${extraHeader}"` },
+        { name: "origin", command: `remote add origin ${repoUrl}` },
+        { name: "fetch", command: `fetch origin ${options.CommitHash} --depth=1` },
+        { name: "tag", command: `tag -a "${options.TagName}"${tagMessageCommand} ${options.CommitHash}${forceCommand}` },
+        { name: "push", command: `push origin "${options.TagName}"${forceCommand}` }
+    ];
+
+    for (var c of orderedCommands) {
+        try {
+            await executor("git", c.command);
+        }
+        catch (e) {
+            return {
+                Succeeded: false,
+                Message: `Error running command '${c.name}': ${(e as Error).message}`,
+                FailedCommand: c.name
+            }
         }
     }
 
-    return function () { return "";}
-}
-
-let execFunction : function(string, string[]) Q.Promise<number>  = tl.exec;
-
-export async function addTag(options: TagOptions) {
-    tl.exec("git", "init")
-
-    const repoUrl = `${options.ServiceUrl}/${options.RepoId}.git`;
-
-    const forceCommand = options.ForcePush === true ? " --force" : "";
-    const tagMessageCommand = options.TagMessage === "" ? "" : `" -m ${options.TagMessage}"`;
-    const commands = [
-        "init",
-        `config user.name "Universal Git Tags"`,
-        "config user.email universal-git-tags@cloudpup.dev",
-        `config http.extraheader "${buildCredentialsFunction(options.Credentials)}"`,
-        `remote add origin ${repoUrl}`,
-        `fetch origin ${options.CommitHash} --depth=1`,
-        `tag -a "${options.TagName}"${tagMessageCommand} ${options.CommitHash}${forceCommand}`,
-        `push origin "${options.TagName}"${forceCommand}`
-    ];
-        
-    for(let command in commands) {
-        await tl.exec("git", commands)
-    }
-
     return {
-        succeeded: true
+        Succeeded: true
     }
 }
